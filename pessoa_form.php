a8b84179e324eb5078fe645746e598f74c5fbfbd
marcosmitsi <marcos.mitsi@hotmail.com>
2025-03-04 20:36:05 -0300

    insert and edit em um unico arquivo

--- pessoa_form.php
+++ pessoa_form.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
@@ -8,71 +9,143 @@
     <link rel='stylesheet' type='text/css' media='screen' href='css/form.css'>
     <script src='main.js'></script>
 </head>
+
 <body>
     <?php
-    if(!empty($_GET['id']))
-    {
+
+    $id = '';
+    $nome = '';
+    $endereco = '';
+    $bairro = '';
+    $telefone = '';
+    $email = '';
+    $id_cidade = '';
+
+
+    if (!empty($_REQUEST['action'])) {
         //Connection
-        $user ="system";
+        $user = "system";
         $password = "243156";
-        $host ="192.168.0.169";
-        $port ="1521";
-        $bd ="XE";
+        $host = "192.168.0.169";
+        $port = "1521";
+        $bd = "XE";
 
         $dsn = "//{$host}:{$port}/{$bd}"; //Construção da DSN (Data Source Name):
         $conn = oci_connect($user, $password, $dsn, 'AL32UTF8'); //Conexão com o banco: DSN e especificando AL32UTF8.
-
+    
         // Garate que o conteúdo seja Utf8
         header('content-type: text/html; charset=UTF-8'); //Essa linha garante que o navegador interprete a página como HTML em UTF-8.
-        if(!$conn) //Verificação de erro na conexão:
+        if (!$conn) //Verificação de erro na conexão:
         {
             $e = oci_error();
-            die("Erro ao Conectar: ". $e['message']);
+            die("Erro ao Conectar: " . $e['message']);
         }
 
-        $id = (int) $_GET['id']; //Obtendo o ID:
-        $sql_id = "SELECT * FROM PESSOA WHERE id ='{$id}'";  //Criação da consulta SQL
-        $result = oci_parse($conn, $sql_id); //Preparação da consulta
-        oci_execute($result); //precisa chamar a função oci_execute() depois de oci_parse() // EXECUTA A CONSULTA:
-        $row = oci_fetch_assoc($result); //Extração dos dados oci_fetch_assoc() tenta buscar um array associativo.
+        if ($_REQUEST['action'] == 'edit') {
 
-        $id       = $row['ID'];
-        $nome     = $row['NOME'];
-        $endereco = $row['ENDERECO'];
-        $bairro   = $row['BAIRRO'];
-        $telefone = $row['TELEFONE'];
-        $email    = $row['EMAIL'];
-        $id_cidade   = $row['ID_CIDADE'];
+            if (!empty($_GET['id'])) {
 
 
-        //print_r($row);
+                $id = (int) $_GET['id']; //Obtendo o ID:
+                $sql_id = "SELECT * FROM PESSOA WHERE id ='{$id}'";  //Criação da consulta SQL
+                $result = oci_parse($conn, $sql_id); //Preparação da consulta
+                oci_execute($result); //precisa chamar a função oci_execute() depois de oci_parse() // EXECUTA A CONSULTA:
+                $row = oci_fetch_assoc($result); //Extração dos dados oci_fetch_assoc() tenta buscar um array associativo.
+    
+                $id = $row['ID'];
+                $nome = $row['NOME'];
+                $endereco = $row['ENDERECO'];
+                $bairro = $row['BAIRRO'];
+                $telefone = $row['TELEFONE'];
+                $email = $row['EMAIL'];
+                $id_cidade = $row['ID_CIDADE'];
 
+
+                //print_r($row);
+    
+            }
+        } elseif ($_REQUEST['action'] == 'save') {
+            $id = $_POST['id'];
+            $nome = $_POST['nome'];
+            $endereco = $_POST['endereco'];
+            $bairro = $_POST['bairro'];
+            $telefone = $_POST['telefone'];
+            $email = $_POST['email'];
+            $id_cidade = $_POST['id_cidade'];
+
+            if (empty($_POST['id'])) {
+                $result = oci_parse($conn, "SELECT MAX(ID) AS NEXT FROM PESSOA");
+                oci_execute($result);
+                $row = oci_fetch_assoc($result);
+                $next = (int) $row['NEXT'] + 1;
+
+                $sql = "INSERT INTO PESSOA (id, nome, endereco, bairro, telefone, email, id_cidade)
+                        VALUES('{$next}','{$nome}','{$endereco}','{$bairro}','{$telefone}','{$email}','{$id_cidade}')";
+
+                $result = oci_parse($conn, $sql);
+                //oci_execute($result);  estamos chamando no IF abaixo
+
+                if (!oci_execute($result)) {
+                    $erro = oci_error($result);
+                    echo "<script>alert('Erro ao inserir: ');</script> " . $erro['message'];
+                } else {
+                    oci_commit($conn);
+                    echo "<script>alert('Registro inserido com sucesso!');</script>";
+                    echo "<script>window.location='pessoa_list.php';</script>"; // Após excluir, a página recarrega para 
+    
+                }
+                oci_free_statement($result);
+                oci_close($conn);
+
+            } else {
+                $sql = "UPDATE PESSOA SET nome      = '{$nome}',
+                                          endereco  = '{$endereco}',
+                                          bairro    = '{$bairro}',
+                                          telefone  = '{$telefone}',
+                                          email     = '{$email}',
+                                          id_cidade = '{$id_cidade}'
+                                          WHERE id  = '{$id}'";
+                $result = oci_parse($conn, $sql);
+                if(!oci_execute($result)){
+                    $erro = oci_error($result);
+                    echo"<script>alert('Erro ao alterar: ')</script>" . $erro['message'];
+                }else{
+                    oci_commit($conn);
+                    echo"<script>alert('Registro alterado com sucesso!')</script>";
+                    echo"<script>window.location ='pessoa_list.php';</script>";
+                }
+                oci_free_statement($result);
+                oci_close($conn);
+
+            }
+        }
     }
-    ?>    
-    <form enctype="multipart/form-data" method="post" action="pessoa_save_update.php">
+    ?>
+    <form enctype="multipart/form-data" method="post" action="pessoa_form.php?action=save">
         <label>Código</label>
-        <input name="id" readonly="1" type="text" style="width:25%" value="<?=$id?>">
+        <input name="id" readonly="1" type="text" style="width:25%" value="<?= $id ?>">
         <label>Nome</label>
-        <input name="nome"  type="text" style="width:50%" value="<?=$nome?>">
+        <input name="nome" type="text" style="width:50%" value="<?= $nome ?>">
         <label>Endereço</label>
-        <input name="endereco" type="text" style="width:50%" value="<?=$endereco?>">
+        <input name="endereco" type="text" style="width:50%" value="<?= $endereco ?>">
         <label>Bairro</label>
-        <input name="bairro" type="text" style="width:25%" value="<?=$bairro?>">
+        <input name="bairro" type="text" style="width:25%" value="<?= $bairro ?>">
         <label>Telefone</label>
-        <input name="telefone" type="text" style="width:25%" value="<?=$telefone?>">
+        <input name="telefone" type="text" style="width:25%" value="<?= $telefone ?>">
         <label>Email</label>
-        <input name="email" type="text" style="width:25%" value="<?=$email?>">
+        <input name="email" type="text" style="width:25%" value="<?= $email ?>">
         <label>Cidade</label>
         <select name="id_cidade" style="width: 25%">
-            <?php 
-            require_once'lista_combo_cidades.php';
+            <?php
+            require_once 'lista_combo_cidades.php';
             print lista_combo_cidade($id_cidade);
             ?>
         </select>
 
         <input type="submit">
-       
+
     </form>
-    
+
 </body>
+
 </html>
--- pessoa_list.php
+++ pessoa_list.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Listagem de Pessoas</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='css/list.css'>
-    
+
 </head>
+
 <body>
     <table border="1">
         <thead>
@@ -25,33 +27,30 @@
         </thead>
         <tbody>
             <?php
-            $user ="system";
-            $password ="243156";
+            $user = "system";
+            $password = "243156";
             $host = "192.168.0.169";
             $port = "1521";
             $bd = "XE";
 
-            $dsn ="//{$host}:{$port}/{$bd}";
+            $dsn = "//{$host}:{$port}/{$bd}";
             $conn = oci_connect($user, $password, $dsn);
 
             //Garante que o cabeçalho de conteúdo seja UTF-8
             header('Content-Type: text/html; charset=UTF-8');
 
-            if(!$conn)
-            {
+            if (!$conn) {
                 $e = oci_error($conn);
-                echo!"".$e['message'];
-            }
-            else
-            {
+                echo !"" . $e['message'];
+            } else {
                 //echo "Conexão bem-sucedida!<br>";
             }
 
             if (!empty($_GET['action']) && $_GET['action'] == 'delete') {
                 $id = (int) $_GET['id'];
                 $sql = "DELETE FROM PESSOA WHERE id = :id";
-            
-                
+
+
                 $stmt = oci_parse($conn, $sql); //Prepara a query para execução.
             
                 oci_bind_by_name($stmt, ':id', $id); //Associa o valor id na query (segurança contra SQL Injection).
@@ -66,37 +65,35 @@
                     echo "Erro ao excluir registro: " . $e['message'];
                 }
             }
-            
+
 
 
             $sql = "SELECT * FROM PESSOA ORDER BY id";
             $result = oci_parse($conn, $sql);
-            
-            if (!oci_execute($result)) 
-            {
+
+            if (!oci_execute($result)) {
                 $e = oci_error($result);
                 die("Erro ao executar consulta: " . $e['message']);
             }
-            
+
             // Busca os dados corretamente
-            while ($row = oci_fetch_assoc($result)) 
-            {
-             /***O Oracle pode estar armazenando os dados em um charset diferente (ISO-8859-1, WE8ISO8859P1, WE8MSWIN1252, etc.).mb_convert_encoding($value, 'UTF-8', 'ISO-8859-1') converte para UTF-8 e corrige caracteres estranhos. */
+            while ($row = oci_fetch_assoc($result)) {
+                /***O Oracle pode estar armazenando os dados em um charset diferente (ISO-8859-1, WE8ISO8859P1, WE8MSWIN1252, etc.).mb_convert_encoding($value, 'UTF-8', 'ISO-8859-1') converte para UTF-8 e corrige caracteres estranhos. */
                 foreach ($row as $key => $value) {
                     //Dessa forma, se o valor for null, ele não tentará convertê-lo
                     if ($value !== null) {
-                    $row[$key] = mb_convert_encoding($value, 'UTF-8', 'ISO-8859-1');
+                        $row[$key] = mb_convert_encoding($value, 'UTF-8', 'ISO-8859-1');
                     }
                 }
 
-                $id        = $row['ID'];
-                $nome      = $row['NOME'];
-                $endereco  = $row['ENDERECO'];
-                $bairro    = $row['BAIRRO'];
-                $telefone  = $row['TELEFONE'];
-                $email     = $row['EMAIL'];
+                $id = $row['ID'];
+                $nome = $row['NOME'];
+                $endereco = $row['ENDERECO'];
+                $bairro = $row['BAIRRO'];
+                $telefone = $row['TELEFONE'];
+                $email = $row['EMAIL'];
                 $id_cidade = $row['ID_CIDADE'];
-            
+
                 print '<tr>';
                 print "<td><a href='pessoa_form.php?action=edit&id={$id}'>
                             <img src='images/edit.svg' style='width:17px'>
@@ -113,14 +110,15 @@
                 print "<td> {$id_cidade} </td>";
                 print '</tr>';
             }
-            
+
             oci_close($conn);
 
             ?>
         </tbody>
-    </table> 
+    </table>
     <button onclick="window.location='pessoa_form.php'">
         <img src="images/insert.svg" style="width: 17px;">Inserir
-    </button>       
+    </button>
 </body>
+
 </html>
