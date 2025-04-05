19ac79a3ea9702b20bb038d85b0e800dbdfd7553
marcosmitsi <marcos.mitsi@hotmail.com>
2025-04-05 15:45:45 -0300

    Conexoes e Seguranca

--- classes/Pessoa.php
+++ classes/Pessoa.php
@@ -2,118 +2,102 @@
 
 class Pessoa
 {
+    private static $conn;
+    public static function getConnection()
+    {
+
+        $ini = parse_ini_file('./config/xe.ini');
+
+        $host = $ini['host'];  // IP do servidor Oracle
+        $porta = $ini['porta'];          // Porta padrão do Oracle
+        $servicename = $ini['servicename'];      // Nome do serviço ou SID
+        $usuario = $ini['usuario']; // Usuário do banco
+        $senha = $ini['senha'];     // Senha do banco
+
+        // DSN (Data Source Name) para Oracle com Service Name
+        $dsn = "oci:dbname=//$host:$porta/$servicename;charset=UTF8";
+
+
+
+        // Criando a conexão PDO
+        if (empty(self::$conn)) {
+            self::$conn = new PDO($dsn, $usuario, $senha, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Ativar erros como exceções
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Retornar arrays associativos
+
+            ]);
+        }
+
+        return self::$conn;
+
+
+    }
     public static function find($id)
     {
-        $host = "192.168.0.169";
-        $porta = "1521";
-        $bd = "XE";
-        $usuario = "system";
-        $senha = "243156";
+        $conn = self::getConnection();
+        $result = $conn->prepare("SELECT * FROM pessoa WHERE id = :id");
+        $result->execute([':id' => $id]);
+        return $result->fetch();
 
-        try {
-            $dsn = "oci:dbname=//$host:$porta/$bd;charset=UTF8";
-            $conn = new PDO($dsn, $usuario, $senha, [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            ]);
-            $result = $conn->query("SELECT * FROM pessoa WHERE id = '{$id}'");
-            return $result->fetch();
-
-        } catch (PDOException $e) {
-            echo "Erro na conexão: " . $e->getMessage();
-        }
 
     }
 
     public static function delete($id)
     {
-        $host = "192.168.0.169";
-        $porta = "1521";
-        $bd = "XE";
-        $usuario = "system";
-        $senha = "243156";
+        $conn = self::getConnection();
+        $result = $conn->prepare("DELETE FROM pessoa WHERE id = :id");
+        $result->execute([':id' => $id]);
+        return $result;
 
-        try {
-            $dsn = "oci:dbname=//$host:$porta/$bd;charset=UTF8";
-            $conn = new PDO($dsn, $usuario, $senha, [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            ]);
-            $result = $conn->query("DELETE FROM pessoa WHERE id = '{$id}'");
-            return $result;
-
-        } catch (PDOException $e) {
-            echo "Erro na conexão: " . $e->getMessage();
-        }
 
     }
 
     public static function all()
     {
-        $host = "192.168.0.169";
-        $porta = "1521";
-        $bd = "XE";
-        $usuario = "system";
-        $senha = "243156";
 
-        try {
-            $dsn = "oci:dbname=//$host:$porta/$bd;charset=UTF8";
-            $conn = new PDO($dsn, $usuario, $senha, [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            ]);
-            $result = $conn->query("SELECT * FROM pessoa ORDER BY id");
-            return $result->fetchAll();
+        $conn = self::getConnection();
+        $result = $conn->query("SELECT * FROM pessoa ORDER BY id");
+        return $result->fetchAll();
 
-        } catch (PDOException $e) {
-            echo "Erro na conexão: " . $e->getMessage();
-        }
 
     }
 
     public static function save($pessoa)
     {
-        $host = "192.168.0.169";
-        $porta = "1521";
-        $bd = "XE";
-        $usuario = "system";
-        $senha = "243156";
+        $conn = self::getConnection();
 
-      
-            $dsn = "oci:dbname=//$host:$porta/$bd;charset=UTF8";
-            $conn = new PDO($dsn, $usuario, $senha, [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            ]);
+        if (empty($pessoa['id'])) {
+            $result = $conn->query("SELECT MAX(id) AS NEXT FROM pessoa");
+            $row = $result->fetch();
+            $pessoa['id'] = (int) $row['NEXT'] + 1;
+            $sql = "INSERT INTO PESSOA (id, nome, endereco, bairro, telefone, email, id_cidade)
+                        VALUES(:id, :nome, :endereco, :bairro, :telefone, :email, :id_cidade)";
 
-            if (empty($pessoa['id'])) {
-                $result = $conn->query("SELECT MAX(id) AS NEXT FROM pessoa");
-                $row = $result->fetch();
-                $pessoa['id'] = (int) $row['NEXT'] + 1 ;
-                $sql = "INSERT INTO PESSOA (id, nome, endereco, bairro, telefone, email, id_cidade)
-                        VALUES('{$pessoa['id']}',
-                               '{$pessoa['nome']}',
-                               '{$pessoa['endereco']}',
-                               '{$pessoa['bairro']}',
-                               '{$pessoa['telefone']}',
-                               '{$pessoa['email']}',
-                               '{$pessoa['id_cidade']}')";
+        } else {
+            $sql = "UPDATE PESSOA SET     nome      = :nome,
+                                          endereco  = :endereco,
+                                          bairro    = :bairro,
+                                          telefone  = :telefone,
+                                          email     = :email,
+                                          id_cidade = :id_cidade
+                                    WHERE id        = :id";
 
-            } else {
-                $sql = "UPDATE PESSOA SET nome      = '{$pessoa['nome']}',
-                                          endereco  = '{$pessoa['endereco']}',
-                                          bairro    = '{$pessoa['bairro']}',
-                                          telefone  = '{$pessoa['telefone']}',
-                                          email     = '{$pessoa['email']}',
-                                          id_cidade = '{$pessoa['id_cidade']}'
-                                    WHERE id        = '{$pessoa['id']}'";
+        }
 
-            }
+        $result = $conn->prepare($sql);
+        $result->execute([
+            ':id' => $pessoa['id'],
+            ':nome' => $pessoa['nome'],
+            ':endereco' => $pessoa['endereco'],
+            ':bairro' => $pessoa['bairro'],
+            ':telefone' => $pessoa['telefone'],
+            ':email' => $pessoa['email'],
+            ':id_cidade' => $pessoa['id_cidade']
 
-            return $conn->query($sql);
+        ]);
 
 
-        
+
     }
 
 
